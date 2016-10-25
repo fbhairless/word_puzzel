9154231a5d0ff899ea28c29c2c45eaacc1c4790c
Hal Fairless and Jared Alford <jared@>
2016-10-25 11:40:30 -0700

    Created a word puzzle.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  var str = "Believe you can and you're halfway there. Theodore Roosevelt";
+  var newStr = str.split("");
+  for (index=0; index<newStr.length; index+=1) {
+  	if (newStr[index] ==="a" || newStr[index] ==="e" || newStr[index] ==="i" || newStr[index] ==="o" || newStr[index] ==="u")
+        newStr.splice(index, 1, "-");
+    }
+   var final = newStr.join("");
+    alert(final);
+
+
+    event.preventDefault();
+  });
